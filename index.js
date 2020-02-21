5b2c97dc8e60d7d369d91041eeb674e4e466c649
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-02-21 13:11:55 -0800

    fixing messages

--- index.js
+++ index.js
@@ -73,25 +73,57 @@ function takeOrder(){
     }else if (order === 'console') {
         item = '<img src="https://media.wired.com/photos/5926f81af3e2356fd800b0ac/master/w_1098,c_limit/xbox-one-microsoft.jpg: >';
     }else if (order === 'toys') {
-        item = 'img src="https://s1.thcdn.com/productimg/1600/1600/12054207-1574653303040860.jpg" >';
+        item = '<img src="https://s1.thcdn.com/productimg/1600/1600/12054207-1574653303040860.jpg" >';
     }else {
         item = '<strong>No items ordered</strong>';
     }
     var myItem = item
-    document.write(myItem);
+    // document.write(myItem);
     return myItem;
 };
 
-function howMany(){
-    var person = prompt('how many games do you want?')
-    var response;
+//at home attempt loop
 
-    var i; 
-    for (i= 0; i < (response); i++){
-        document.write(i);
-        }
+// function howMany(){
+//     var person = prompt('how many games do you want?')
+//     var response;
 
-    var userResponse = response;
-    document.write('write');
-    return userResponse;
+//     var i; 
+//     for (i= 0; i < (response); i++){
+//         document.write(i);
+//         }
+
+//     var userResponse = response;
+//     document.write('write');
+//     return userResponse;
+// }
+
+
+//in class walk through loop
+var games = function(){
+
+    var item= takeOrder();
+    var order= prompt ("how many games do you want?");
+    var total = "";
+    // var item= takeOrder();
+
+    console.log(typeof(order));
+    console.log(isNaN(order));
+
+    // var num = Number(order);
+
+    while (order === '0' || isNaN(order)){
+        order = prompt("Please enter a number greater than 0.");
+    }
+    var num = Number(order);
+
+    for(var i = 0; i < order; i++){
+        total = total + item
+    }
+
+    return total;
 }
+// in html
+// call and invoke in head
+//<script> document.write (coffee()) </script>
+
